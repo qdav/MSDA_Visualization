89cd2385cb3ea4912f75a1f46af8d411c61c7def
Quinn McIntire <quinnmac@swbell.net>
2018-08-25 11:57:42 -0500

    experimented with state map of permits

--- ncis_firearm_data.R
+++ ncis_firearm_data.R
@@ -1,14 +1,32 @@
 #install.packages("tidyverse")
 #install.packages("zoo")
+#install.packages("usmap")
+
 
 library(tidyverse)
 library(zoo)
+library(usmap)
 
 options(stringsAsFactors = FALSE)
 
+# retrieve data from comma delimited file/adjust data types
 permits <- read.csv("nics-firearm-background-checks.csv")
 permits$month <- as.yearmon(permits$month)
 
+
+
+# show map of permits by state for 2017
+permits2017 <- filter(permits, 
+                       permits$month >= as.yearmon("2017-01") &
+                       permits$month < as.yearmon("2018-01"))
+
+usmap::plot_usmap(data = permits2017, values = "totals", lines = "red") + 
+  scale_fill_continuous(
+    low = "white", high = "red", name = "Permits By State 2017", label = scales::comma
+  ) + theme(legend.position = "right")
+
+
+# texas permits over time
 texas_perm <- filter(permits, permits$state == "Texas")
 
 ggplot(data = texas_perm) + 
