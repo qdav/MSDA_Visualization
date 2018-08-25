7d5644ccf5d0e241a548c4f572ec36b752a19344
Quinn McIntire <quinnmac@swbell.net>
2018-08-25 08:45:26 -0500

    adding comments

--- Quandl.R
+++ Quandl.R
@@ -1,5 +1,9 @@
+#install.packages("Quandl")
+
+# Quinn's api_key being used here
 Quandl.api_key("awJDWs_WvUiV6VWC8rLb")
 library(Quandl)
+
 #mydata = Quandl("FRED/GDP")
 mydata = Quandl('ZILLOW/C25709_ZRISFRR')
 View(mydata)
