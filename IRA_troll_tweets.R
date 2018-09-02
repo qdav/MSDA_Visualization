8d0d2b9c2f47f99debc90b8148d08ec64c143681
Quinn McIntire <quinnmac@swbell.net>
2018-09-01 21:20:00 -0500

    sentiment analysis on 3M data set

--- IRA_troll_tweets.R
+++ IRA_troll_tweets.R
@@ -29,28 +29,85 @@ library(tidyverse)
 library(syuzhet)
 
 
+# read in tweet data files and combine
 tweet_list <- list() 
 listcsv <- dir(pattern = "IRAhandle_tweets_.") 
 for (k in 1:length(listcsv)){
   tweet_list[[k]] <- read.csv(listcsv[k], sep=',', stringsAsFactors=F)
 }
 
-tweets <- bind_rows (tweet_list)
+tweets <- bind_rows(tweet_list) #combine all the tweet file data
+str(tweets)
 
 tweets$publish_date<-mdy_hm(tweets$publish_date)
+tweets$publish_day<-as.Date(tweets$publish_date)
 tweets$harvested_date<-mdy_hm(tweets$harvested_date)
 tweets$weekdays<-weekdays(tweets$publish_date)
 tweets$weekdays <- factor(tweets$weekdays, levels = rev(c("Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday", "Sunday")))
-View(tweets)
 
-tweets_subset <- tweets[1:10000,]
-tweets_vector <- tweets_subset$content
-#a <- tweets_vector
+#control how many tweets you work with
+tweets_sub <- tweets[1:1000,]
 
-a_corpus = Corpus(VectorSource(tweets_vector))
-a_tm <- TermDocumentMatrix(a_corpus)
-a_tmx <- as.matrix(a_tm)
-a_df<-data.frame(text=unlist(sapply(a, `[`)), stringsAsFactors=F)
-a_df$nrc_sent<-get_nrc_sentiment(a_df$text) 
-View(a_df)
-colSums(a_df)
+# get sentiment and create columns in tweet data set
+tweets_sub$nrc_sentiment <- get_nrc_sentiment(tweets_sub$content)
+tweets_sub$anger <- tweets_sub$nrc_sentiment$anger
+tweets_sub$anticipation <- tweets_sub$nrc_sentiment$anticipation
+tweets_sub$disgust <- tweets_sub$nrc_sentiment$disgust
+tweets_sub$fear <- tweets_sub$nrc_sentiment$fear
+tweets_sub$joy <- tweets_sub$nrc_sentiment$joy
+tweets_sub$sadness <- tweets_sub$nrc_sentiment$sadness
+tweets_sub$surprise <- tweets_sub$nrc_sentiment$surprise
+tweets_sub$trust <- tweets_sub$nrc_sentiment$trust
+tweets_sub$negative <- tweets_sub$nrc_sentiment$negative
+tweets_sub$positive <- tweets_sub$nrc_sentiment$positive
+tweets_sub$nrc_sentiment <- NULL
+
+
+# total author sentiment 
+author_sentiment <- select(tweets_sub, author, anger,
+                           anticipation, disgust, fear, joy, 
+                           sadness, surprise, trust, negative, positive) %>% 
+  group_by(author) %>% 
+  summarise(
+    anger = sum(anger, na.rm = TRUE),
+    anticipation = sum(anticipation, na.rm = TRUE),
+    disgust = sum(disgust, na.rm = TRUE),
+    fear = sum(fear, na.rm = TRUE),
+    joy = sum(joy, na.rm = TRUE),
+    sadness = sum(sadness, na.rm = TRUE),
+    surprise = sum(surprise, na.rm = TRUE),
+    trust = sum(trust, na.rm = TRUE),
+    negative = sum(negative, na.rm = TRUE),
+    positive = sum(positive, na.rm = TRUE), 
+    tweet_count = n()
+  )
+View(author_sentiment)
+
+
+#sentiment over time
+
+
+sentiment_over_time <- select(tweets_sub, publish_day,
+                           anger, anticipation, disgust, fear, joy, 
+                           sadness, surprise, trust, negative, 
+                           positive ) %>%
+  group_by(publish_day) %>% 
+  summarise(
+    anger = sum(anger, na.rm = TRUE),
+    anticipation = sum(anticipation, na.rm = TRUE),
+    disgust = sum(disgust, na.rm = TRUE),
+    fear = sum(fear, na.rm = TRUE),
+    joy = sum(joy, na.rm = TRUE),
+    sadness = sum(sadness, na.rm = TRUE),
+    surprise = sum(surprise, na.rm = TRUE),
+    trust = sum(trust, na.rm = TRUE),
+    negative = sum(negative, na.rm = TRUE),
+    positive = sum(positive, na.rm = TRUE), 
+    tweet_count = n()
+  )
+  
+View(sentiment_over_time)
+
+ggplot(data = sentiment_over_time) + 
+  geom_line(mapping = aes(x = publish_day, y = positive, color="Positive")) +
+  geom_line(mapping = aes(x = publish_day, y = negative, color="Negative"))
